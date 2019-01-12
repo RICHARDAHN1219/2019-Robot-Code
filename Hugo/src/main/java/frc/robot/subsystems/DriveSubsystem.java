e0373790ef37692fe3cf7106fa9ae87976d01d86
SonicSquirrels <sonicsquirrels@gmail.com>
2019-01-11 20:17:34 -0800

    initial hugo code

---
+++ Hugo/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import frc.robot.Robot;
+import frc.robot.commands.DriveCommand;
+
+
+/**
+ * Add your docs here.
+ */
+public class DriveSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  public static final Spark motorLeft =new Spark(1);
+  public static final Spark motorRight = new Spark(0);
+  public static DifferentialDrive drive = new DifferentialDrive(motorLeft, motorRight);
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(new DriveCommand());
+  }
+
+  public void Drive(double xSpeed, double zRotation) {
+    drive.arcadeDrive(xSpeed, zRotation);
+  }
+}

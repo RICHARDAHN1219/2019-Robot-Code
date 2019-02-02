efaa9c662820d5ca1028741462ee2ff4de1ce0df
Sonic Squirrels <sonicsquirrels@gmail.com>
2019-02-01 16:40:46 -0800

    gyro subsystem

--- NEO experiment/src/main/java/frc/robot/subsystems/gyroSubsystem.java
+++ NEO experiment/src/main/java/frc/robot/subsystems/gyroSubsystem.java
@@ -14,21 +14,45 @@ import edu.wpi.first.wpilibj.SPI;
 /**
  * Subsystem for Gyro.
  */
+
 public class gyroSubsystem extends Subsystem {
+  
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   private AHRS gyro = new AHRS(SPI.Port.kMXP);
-
+  private boolean RoborioHorizontal = false;
+  /* 
+  Roborio horizontal when laying flat, vertical when laying vertical
+  */
+  private boolean RoborioVertical = true;
+  
+//Returns in degrees https://www.kauailabs.com/public_files/navx-mxp/apidocs/java/com/kauailabs/navx/frc/AHRS.html   class AHRS
+ 
   // TODO:
   // init function to rest Gyro
-  // getYaw function
+  // getRoll function
   // get yaw error, return -180 to 180 error off of desired yaw
+ public void setRoborioVertical() {
+   RoborioVertical = true;
+ }
+  public void setRoborioHorizontal() {
+    RoborioHorizontal = true;
+  }
+  public float getAngle() {
+    if RoborioHorizontal {
+      return gyro.getYaw();
+    }
+    else {
+      return getRoll();
+    }
+  } 
+  
 
   @Override
   public void initDefaultCommand() {
+    
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
 
     gyro.reset();
   }
-}

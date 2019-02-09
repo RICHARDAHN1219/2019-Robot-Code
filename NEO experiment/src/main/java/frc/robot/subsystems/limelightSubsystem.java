b1a114a2cc88dfb1ca74c7f72dba2fd49c169add
Bryn Dole <bryn.dole@gmail.com>
2019-02-09 13:53:04 -0800

    add vision target, renumber pipelines

--- NEO experiment/src/main/java/frc/robot/subsystems/limelightSubsystem.java
+++ NEO experiment/src/main/java/frc/robot/subsystems/limelightSubsystem.java
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project. */
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
@@ -19,24 +19,32 @@ public class limelightSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   public NetworkTable table;
-  
-    //limelightName is the networktable name. For example "limelight-zero"
-    @Override    
-    public void initDefaultCommand() {
-      // Set the default command for a subsystem here.
-      // setDefaultCommand(new MySpecialCommand());
-      setDefaultCommand(null);
-    }
-  public void setlimelightName(String limelightName){
+
+  // limelightName is the networktable name. For example "limelight-zero"
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(null);
+  }
+
+  public void setlimelightName(String limelightName) {
     table = NetworkTableInstance.getDefault().getTable(limelightName);
   }
-  public void setCargoPipeline() {
-  NetworkTableEntry pipeline = table.getEntry("pipeline");
-  pipeline.setNumber(0);
+
+  public void setVisionTargetPipeline() {
+    NetworkTableEntry pipeline = table.getEntry("pipeline");
+    pipeline.setNumber(0);
   }
-  public void setTargetPipeline() {
+
+  public void setCargoPipeline() {
     NetworkTableEntry pipeline = table.getEntry("pipeline");
     pipeline.setNumber(1);
-    }
   }
+
+  public void setTargetPipeline() {
+    NetworkTableEntry pipeline = table.getEntry("pipeline");
+    pipeline.setNumber(2);
+  }
+}
 

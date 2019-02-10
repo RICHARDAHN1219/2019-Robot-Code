e8d62272a23f44c10c093ae4371c6f535c5ed80d
Bryn Dole <bryn.dole@gmail.com>
2019-02-10 14:25:26 -0800

    formatting and whitespace

--- NEO experiment/src/main/java/frc/robot/subsystems/armSubsystem.java
+++ NEO experiment/src/main/java/frc/robot/subsystems/armSubsystem.java
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project. */
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
@@ -25,17 +25,17 @@ public class armSubsystem extends Subsystem {
   private int kPIDLoopIdx = 0;
   private int kTimeoutMs = 30;
   private double kP = 0.15;
-	private double kI = 0.0;
-	private double kD = 1.0;
+  private double kI = 0.0;
+  private double kD = 1.0;
   private double kF = 0.0;
 
 
-  
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
-    armDrive.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, kPIDLoopIdx, kTimeoutMs);
+    armDrive.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, kPIDLoopIdx,
+        kTimeoutMs);
     /* Ensure sensor is positive when output is positive */
     armDrive.setSensorPhase(true);
     armDrive.setInverted(false);
@@ -46,19 +46,19 @@ public class armSubsystem extends Subsystem {
     armDrive.configPeakOutputReverse(-1, kTimeoutMs);
     armDrive.configAllowableClosedloopError(0, kPIDLoopIdx, kTimeoutMs);
     /* Config Position Closed Loop gains in slot0, tsypically kF stays zero. */
-    armDrive.config_kF( kPIDLoopIdx,  kF,  kTimeoutMs);
-		armDrive.config_kP( kPIDLoopIdx,  kP,  kTimeoutMs);
-		armDrive.config_kI( kPIDLoopIdx,  kI,  kTimeoutMs);
-    armDrive.config_kD( kPIDLoopIdx,  kD,  kTimeoutMs);
+    armDrive.config_kF(kPIDLoopIdx, kF, kTimeoutMs);
+    armDrive.config_kP(kPIDLoopIdx, kP, kTimeoutMs);
+    armDrive.config_kI(kPIDLoopIdx, kI, kTimeoutMs);
+    armDrive.config_kD(kPIDLoopIdx, kD, kTimeoutMs);
     /**
-		 * Grab the 360 degree position of the MagEncoder's absolute
-		 * position, and intitally set the relative sensor to match.
-		 */
+     * Grab the 360 degree position of the MagEncoder's absolute position, and intitally set the
+     * relative sensor to match.
+     */
     startPosition = armDrive.getSensorCollection().getPulseWidthPosition();
-    
+
     /* Mask out overflows, keep bottom 12 bits. Value will be 0-4096 */
-		startPosition &= 0xFFF;
-    armDrive.setSelectedSensorPosition(startPosition,  kPIDLoopIdx,  kTimeoutMs);
+    startPosition &= 0xFFF;
+    armDrive.setSelectedSensorPosition(startPosition, kPIDLoopIdx, kTimeoutMs);
 
     printDebug();
   }
@@ -71,15 +71,15 @@ public class armSubsystem extends Subsystem {
     _sb.append("ARM out:");
     double motorOutput = armDrive.getMotorOutputPercent();
     _sb.append((int) (motorOutput * 100));
-    _sb.append("%");	// Percent
+    _sb.append("%"); // Percent
     _sb.append("\tpos:");
-		_sb.append(armDrive.getSelectedSensorPosition(0));
-    _sb.append("u"); 	// Native units
+    _sb.append(armDrive.getSelectedSensorPosition(0));
+    _sb.append("u"); // Native units
     _sb.append("\ttarget:");
-		_sb.append(targetPositionRotations);
-    _sb.append("u");	/// Native Units
+    _sb.append(targetPositionRotations);
+    _sb.append("u"); /// Native Units
     System.out.println(_sb);
     /* Reset built string for next loop */
-		_sb.setLength(0);
+    _sb.setLength(0);
   }
 }

072c2c1acc87a2b3a61907b3f05c798cae34f861
Gabriel McMillan <fearxzombie@icloud.com>
2019-02-13 18:17:31 -0800

    This code will do absolutely nothing whatsoever.
    Had a family matter to attend to. Added files and base code(ish).

---
+++ PracticeBot/src/main/java/frc/robot/commandGroups/climbLevel3.java
@@ -0,0 +1,21 @@
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commandGroups;
+
+import frc.robot.Robot;
+import frc.robot.commands.climberDriveCommand;
+
+public class climbLevel3  {
+
+  public climbLevel3() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+
+    // targetPossition, kP, kI, kP, kF
+   addSequential();
+  }
+}

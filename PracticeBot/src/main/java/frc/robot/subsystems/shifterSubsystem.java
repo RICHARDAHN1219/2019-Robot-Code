b6cf955dfef4ce49357777245fca9a9968fa5df9
SonicSquirrels <sonicsquirrels@gmail.com>
2019-02-16 12:06:21 -0800

    Pushed shift modifications and fixes

--- PracticeBot/src/main/java/frc/robot/subsystems/shifterSubsystem.java
+++ PracticeBot/src/main/java/frc/robot/subsystems/shifterSubsystem.java
@@ -25,10 +25,10 @@ public class shifterSubsystem extends Subsystem {
 
   public void highGear() {
     // TODO: need to check if this is the correct direction for high gear
-    Robot.shifterSolenoid.set(DoubleSolenoid.Value.kReverse);
+    Robot.shifterSolenoid.set(DoubleSolenoid.Value.kForward);
   }
 
   public void lowGear() {
-    Robot.shifterSolenoid.set(DoubleSolenoid.Value.kForward);
+    Robot.shifterSolenoid.set(DoubleSolenoid.Value.kReverse);
   }
 }
